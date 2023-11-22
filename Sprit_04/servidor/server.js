f24fe62170a4ff1f103c29140a140fb690c5da45
wfnar <wfnaranjo17@gmail.com>
2023-11-21 20:11:31 -0500

    avance sprint 4

--- Sprit_04/servidor/server.js
+++ Sprit_04/servidor/server.js
@@ -6,9 +6,67 @@ const router = require('./productos/productos');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const { mongoClient } = require('mongodb');
 
 server.use(express.json());
 server.use(router);
+server.use(cors({
+    origin: "*"
+}));
+
+const connection = new mongoClient("mongodb://localhost:27017");
+
+async function iniciarDB() {
+    try {
+      let user = {
+        nombre: "Felipe",
+        apellido: "Naranjo",
+      };
+  
+      await connection.connect();
+  
+      await connection.db("ecommerce").collection("users").insertOne(user);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  iniciarDB();
+  
+  server.use(express.static(path.resolve("./build")));
+  server.get("/", (req, res) => {
+    res.sendFile(path.resolve("./build/index.html"));
+  });
+  
+  server.get("/app", (req, res) => {
+    res.sendFile(path.resolve("./build/index.html"));
+  });
+  server.get("/app", (req, res) => {
+    res.sendFile(path.resolve("./build/index.html"));
+  });
+  
+  server.use(express.json()); // Para leer json
+  server.use(cors()); // Permitir todas las conexione
+  
+  server.get("/users", async (req, res) => {
+    let resultado = await connection
+      .db("ecommerce")
+      .collection("users")
+      .find({ name: "Felipe" })
+      .toArray();
+  
+    res.send(JSON.stringify({ resultado: resultado }));
+  });
+  
+  server.get("/productos", (req, res) => {
+    res.sendFile(path.resolve("./productos.html"));
+  });
+  server.post("/productos", async (req, res) => {
+    let result = await connection.db("icetex").collection("productos").insertOne(req.body);
+  
+    res.send("Se guardo el producto");
+  });
+
 
 server.get("/home", (req, res) => {
     res.send(JSON.stringify({ user : "Hola Felipe"}));
@@ -42,5 +100,5 @@ server.post("/login", (req, res) => {
 
 
 server.listen(3006, () => {
-    console.log("servidor inicializado!!");
+    console.log("servidor inicializado en el puerto 3006!!");
 });
